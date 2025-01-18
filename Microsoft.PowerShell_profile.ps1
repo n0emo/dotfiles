05ee7ed3fbcaa98b58c172510d11901a7370f7cb
Albert Shefner <solaris945@gmail.com>
2025-01-19 02:33:21 +0300

    Add Visual Studio profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,20 @@
+function Invoke-Environment {
+    param
+    (
+        # Any cmd shell command, normally a configuration batch file.
+        [Parameter(Mandatory=$true)]
+        [string] $Command
+    )
+
+    $Command = "`"" + $Command + "`""
+    cmd /c "$Command > nul 2>&1 && set" | . { process {
+        if ($_ -match '^([^=]+)=(.*)') {
+            [System.Environment]::SetEnvironmentVariable($matches[1], $matches[2])
+        }
+    }}
+}
+
+Invoke-Environment "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+
 oh-my-posh init pwsh | Invoke-Expression
+
