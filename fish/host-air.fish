b35913acc330a00e7e6c87dc756ce18adb82965a
Albert Shefner <dev_n0emo@tuta.io>
2025-02-11 02:26:41 +0500

    Update fish/host-air.fish

--- fish/host-air.fish
+++ fish/host-air.fish
@@ -1,5 +1,12 @@
-set -U fish_user_paths $fish_user_paths \
-    $HOME/.juliaup/bin
+set -x FLYCTL_INSTALL $HOME/.fly
+set -x VCPKG_ROOT $HOME/Software/vcpkg
+
+set -U fish_user_paths \
+    /opt/homebrew/bin \
+    $HOME/.juliaup/bin \
+    $FLYCTL_INSTALL/bin \
+    $VCPKG_ROOT \
+    $fish_user_paths
 
 source "$HOME/.cargo/env.fish"
 
