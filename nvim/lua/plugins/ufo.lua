181050c3f400f89564e3f69a2991611d4b875c9a
Albert Shefner <dev_n0emo@tuta.io>
2025-02-11 21:43:17 +0300

    Remove fold line

--- nvim/lua/plugins/ufo.lua
+++ nvim/lua/plugins/ufo.lua
@@ -7,8 +7,7 @@ return {
   config = function ()
     local ufo = require("ufo")
 
-    vim.o.foldcolumn = '1' -- '0' is not bad
-    vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+    vim.o.foldcolumn = '0'
     vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
     vim.o.foldlevelstart = 99
     vim.o.foldenable = true
