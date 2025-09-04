490e73d60357c3efc5587d3241896b34b6026aa7
Albert Shefner <usn0emo@gmail.com>
2025-09-04 09:43:43 +0300

    Add colima env

---
+++ fish/conf.d/colima.fish
@@ -0,0 +1,3 @@
+export DOCKER_HOST="unix://$HOME/.colima/default/docker.sock"
+export TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE="/var/run/docker.sock"
+export TESTCONTAINERS_HOST_OVERRIDE=$(colima ls -j | jq -r '.address')
