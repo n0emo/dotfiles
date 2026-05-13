9de170fa79d855dd50a17e7e2f7fbd59f29c4b89
Albert Shefner <usn0emo@gmail.com>
2026-05-13 18:06:14 +0300

    Update something idk

--- nvim/init.lua
+++ nvim/init.lua
@@ -223,7 +223,31 @@ function install_plugins()
     default_format_opts = {
       lsp_format = "fallback",
     },
-    format_on_save = { timeout_ms = 500 },
+    format_on_save = function(bufnr)
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+      return { timeout_ms = 500, lsp_format = "fallback" }
+    end,
+  })
+
+  vim.api.nvim_create_user_command("FormatDisable", function(args)
+    if args.bang then
+      -- FormatDisable! will disable formatting just for this buffer
+      vim.b.disable_autoformat = true
+    else
+      vim.g.disable_autoformat = true
+    end
+  end, {
+    desc = "Disable autoformat-on-save",
+    bang = true,
+  })
+  vim.api.nvim_create_user_command("FormatEnable", function()
+    vim.b.disable_autoformat = false
+    vim.g.disable_autoformat = false
+  end, {
+    desc = "Re-enable autoformat-on-save",
   })
 
   require("lualine").setup({
@@ -256,6 +280,7 @@ function configure_lsp()
     "jinja_lsp",
     "just",
     "lua_ls",
+    "ols",
     "pyright",
     "rust_analyzer",
     "svelte",
@@ -400,6 +425,7 @@ function setup_treesitter()
     "haskell",
     "javascript",
     "lua",
+    "odin",
     "python",
     "rust",
     "svelte",
